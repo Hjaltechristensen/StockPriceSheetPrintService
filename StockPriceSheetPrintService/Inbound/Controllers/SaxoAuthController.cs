97b0a4a6b6c5eb4672c74aed709d99fcbede3523
Hjaltechristensen <hjalte-christensen@live.dk>
2026-08-03 23:06:29 +0200

    test

--- StockPriceSheetPrintService/Inbound/Controllers/SaxoAuthController.cs
+++ StockPriceSheetPrintService/Inbound/Controllers/SaxoAuthController.cs
@@ -57,7 +57,7 @@ namespace StockPriceSheetPrintService.Inbound.Controllers
 			}
 			catch (Exception ex)
 			{
-				_logger.LogError(ex, "[SAXO-CALLBACK] Error in callback");
+				_logger.LogError(ex, "[SAXO-CALLBACK] Error in callback f");
 				return StatusCode(500, $"Internal server error: {ex.Message}");
 			}
 		}
